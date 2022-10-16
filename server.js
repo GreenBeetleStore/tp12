4a6544a59834c0f0c436183d5ba285443254d865
XT Escarabat Verd <f.xaviertorta@gmail.com>
2022-10-16 15:19:56 +0200

    npm install --save helmet

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const helmet = require("helmet");
 const app = express();
+
+app.use(helmet());
 app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(3000, () => {
  console.log("Le serveur ecoute sur le port 3000");
